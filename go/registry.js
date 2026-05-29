1ac1753137bcd134b4eb068489965e658776379a
lukebrush555-hue <luke.brush555@gmail.com>
2026-05-29 12:09:48 -0400

    Use custom domain paths in SamplePass registry

--- go/registry.js
+++ go/registry.js
@@ -5,7 +5,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     archetype: "claim",
     vendorType: "demo",
     vendorName: "SamplePass Demo",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=SamplePass%20Demo&vendor_id=samplepass-demo&category=Farmers%20Market%20Sample&title=Free%20Sample%20Demo&description=Scan%20to%20see%20how%20a%20market%20sample%20can%20become%20a%20remembered%20customer%20relationship.&cta=Claim%20demo%20sample&limit=Demo%20only.&qr=svfmpa-sp-demo&campaign=svfmpa-samplepass-demo&image=/ropebridge-core/assets/images/claim-image-arriving-soon.svg&image_alt=SamplePass%20demo%20image%20placeholder"
+    destination: "/archetypes/claim/?vendor=SamplePass%20Demo&vendor_id=samplepass-demo&category=Farmers%20Market%20Sample&title=Free%20Sample%20Demo&description=Scan%20to%20see%20how%20a%20market%20sample%20can%20become%20a%20remembered%20customer%20relationship.&cta=Claim%20demo%20sample&limit=Demo%20only.&qr=svfmpa-sp-demo&campaign=svfmpa-samplepass-demo&image=/assets/images/claim-image-arriving-soon.svg&image_alt=SamplePass%20demo%20image%20placeholder"
   },
   "svfmpa-sp-onboard": {
     status: "live",
@@ -13,7 +13,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     archetype: "claim-create",
     vendorType: "onboarding",
     vendorName: "Create Free Sample Page",
-    destination: "/ropebridge-core/archetypes/claim/create/?v=svfmpa-onboard"
+    destination: "/archetypes/claim/create/?v=svfmpa-onboard"
   },
   "svfmpa-sp-001": {
     status: "live",
@@ -22,7 +22,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     vendorType: "coffee",
     vendorName: "Hollow Road Coffee",
     offerTitle: "Cold Brew Tasting",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=Hollow%20Road%20Coffee&vendor_id=hollow-road-coffee&category=Coffee&title=Cold%20Brew%20Tasting&description=Claim%20a%20small%20cold%20brew%20taste%20from%20a%20local%20market%20coffee%20roaster.%20Save%20the%20vendor%20so%20you%20can%20find%20them%20again.&cta=Claim%20cold%20brew%20taste&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-001&campaign=svfmpa-hollow-road-coffee&image=/ropebridge-core/assets/images/faux-vendors/hollow-road-coffee.png&image_alt=Hollow%20Road%20Coffee%20cold%20brew%20sample"
+    destination: "/archetypes/claim/?vendor=Hollow%20Road%20Coffee&vendor_id=hollow-road-coffee&category=Coffee&title=Cold%20Brew%20Tasting&description=Claim%20a%20small%20cold%20brew%20taste%20from%20a%20local%20market%20coffee%20roaster.%20Save%20the%20vendor%20so%20you%20can%20find%20them%20again.&cta=Claim%20cold%20brew%20taste&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-001&campaign=svfmpa-hollow-road-coffee&image=/assets/images/faux-vendors/hollow-road-coffee.png&image_alt=Hollow%20Road%20Coffee%20cold%20brew%20sample"
   },
   "svfmpa-sp-002": {
     status: "live",
@@ -31,7 +31,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     vendorType: "flowers",
     vendorName: "Backyard Blooms",
     offerTitle: "Market Stem Token",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=Backyard%20Blooms&vendor_id=backyard-blooms&category=Flowers&title=Market%20Stem%20Token&description=Claim%20a%20market%20stem%20token%20and%20save%20this%20local%20flower%20vendor%20for%20future%20bouquets%20and%20seasonal%20arrangements.&cta=Claim%20stem%20token&limit=One%20token%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-002&campaign=svfmpa-backyard-blooms&image=/ropebridge-core/assets/images/claim-image-arriving-soon.svg&image_alt=Backyard%20Blooms%20sample%20image%20placeholder"
+    destination: "/archetypes/claim/?vendor=Backyard%20Blooms&vendor_id=backyard-blooms&category=Flowers&title=Market%20Stem%20Token&description=Claim%20a%20market%20stem%20token%20and%20save%20this%20local%20flower%20vendor%20for%20future%20bouquets%20and%20seasonal%20arrangements.&cta=Claim%20stem%20token&limit=One%20token%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-002&campaign=svfmpa-backyard-blooms&image=/assets/images/claim-image-arriving-soon.svg&image_alt=Backyard%20Blooms%20sample%20image%20placeholder"
   },
   "svfmpa-sp-003": {
     status: "live",
@@ -40,7 +40,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     vendorType: "honey",
     vendorName: "Saucon Valley Apiary",
     offerTitle: "Wildflower Raw Honey",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=Saucon%20Valley%20Apiary&vendor_id=saucon-valley-apiary&category=Honey&title=Wildflower%20Raw%20Honey&description=Claim%20a%20small%20taste%20of%20seasonal%20raw%20wildflower%20honey%20from%20local%20Saucon%20Valley%20hives.&cta=Claim%20honey%20taste&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-003&campaign=svfmpa-saucon-valley-apiary&image=/ropebridge-core/assets/images/claim-image-arriving-soon.svg&image_alt=Saucon%20Valley%20Apiary%20sample%20image%20placeholder"
+    destination: "/archetypes/claim/?vendor=Saucon%20Valley%20Apiary&vendor_id=saucon-valley-apiary&category=Honey&title=Wildflower%20Raw%20Honey&description=Claim%20a%20small%20taste%20of%20seasonal%20raw%20wildflower%20honey%20from%20local%20Saucon%20Valley%20hives.&cta=Claim%20honey%20taste&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-003&campaign=svfmpa-saucon-valley-apiary&image=/assets/images/claim-image-arriving-soon.svg&image_alt=Saucon%20Valley%20Apiary%20sample%20image%20placeholder"
   },
   "svfmpa-sp-004": {
     status: "live",
@@ -49,7 +49,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     vendorType: "bakery",
     vendorName: "Crumb & Hearth",
     offerTitle: "Sourdough Sample",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=Crumb%20%26%20Hearth&vendor_id=crumb-and-hearth&category=Bakery&title=Sourdough%20Sample&description=Claim%20a%20bite%20of%20slow-fermented%20market%20sourdough%20and%20save%20the%20bakery%20for%20future%20loaves.&cta=Claim%20sourdough%20bite&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-004&campaign=svfmpa-crumb-and-hearth&image=/ropebridge-core/assets/images/faux-vendors/crumb-and-hearth.png&image_alt=Crumb%20and%20Hearth%20sourdough%20sample"
+    destination: "/archetypes/claim/?vendor=Crumb%20%26%20Hearth&vendor_id=crumb-and-hearth&category=Bakery&title=Sourdough%20Sample&description=Claim%20a%20bite%20of%20slow-fermented%20market%20sourdough%20and%20save%20the%20bakery%20for%20future%20loaves.&cta=Claim%20sourdough%20bite&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-004&campaign=svfmpa-crumb-and-hearth&image=/assets/images/faux-vendors/crumb-and-hearth.png&image_alt=Crumb%20and%20Hearth%20sourdough%20sample"
   },
   "svfmpa-sp-005": {
     status: "live",
@@ -58,7 +58,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     vendorType: "hot-sauce",
     vendorName: "Ember Jar Provisions",
     offerTitle: "Three-Heat Tasting",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=Ember%20Jar%20Provisions&vendor_id=ember-jar-provisions&category=Hot%20Sauce&title=Three-Heat%20Tasting&description=Claim%20a%20small%20mild%2C%20medium%2C%20or%20hot%20sauce%20taste%20and%20save%20your%20favorite%20heat%20level%20for%20later.&cta=Claim%20sauce%20taste&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-005&campaign=svfmpa-ember-jar-provisions&image=/ropebridge-core/assets/images/claim-image-arriving-soon.svg&image_alt=Ember%20Jar%20Provisions%20sample%20image%20placeholder"
+    destination: "/archetypes/claim/?vendor=Ember%20Jar%20Provisions&vendor_id=ember-jar-provisions&category=Hot%20Sauce&title=Three-Heat%20Tasting&description=Claim%20a%20small%20mild%2C%20medium%2C%20or%20hot%20sauce%20taste%20and%20save%20your%20favorite%20heat%20level%20for%20later.&cta=Claim%20sauce%20taste&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-005&campaign=svfmpa-ember-jar-provisions&image=/assets/images/claim-image-arriving-soon.svg&image_alt=Ember%20Jar%20Provisions%20sample%20image%20placeholder"
   },
   "svfmpa-sp-006": {
     status: "live",
@@ -67,7 +67,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     vendorType: "preserves",
     vendorName: "Stone Fence Preserves",
     offerTitle: "Strawberry Rhubarb Jam",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=Stone%20Fence%20Preserves&vendor_id=stone-fence-preserves&category=Jam&title=Strawberry%20Rhubarb%20Jam&description=Claim%20a%20taste%20of%20small-batch%20strawberry%20rhubarb%20jam%20and%20save%20this%20seasonal%20preserve%20maker.&cta=Claim%20jam%20taste&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-006&campaign=svfmpa-stone-fence-preserves&image=/ropebridge-core/assets/images/claim-image-arriving-soon.svg&image_alt=Stone%20Fence%20Preserves%20sample%20image%20placeholder"
+    destination: "/archetypes/claim/?vendor=Stone%20Fence%20Preserves&vendor_id=stone-fence-preserves&category=Jam&title=Strawberry%20Rhubarb%20Jam&description=Claim%20a%20taste%20of%20small-batch%20strawberry%20rhubarb%20jam%20and%20save%20this%20seasonal%20preserve%20maker.&cta=Claim%20jam%20taste&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-006&campaign=svfmpa-stone-fence-preserves&image=/assets/images/claim-image-arriving-soon.svg&image_alt=Stone%20Fence%20Preserves%20sample%20image%20placeholder"
   },
   "svfmpa-sp-007": {
     status: "live",
@@ -76,7 +76,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     vendorType: "cheese",
     vendorName: "Creekside Creamery",
     offerTitle: "Aged Cheddar Bite",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=Creekside%20Creamery&vendor_id=creekside-creamery&category=Cheese&title=Aged%20Cheddar%20Bite&description=Claim%20a%20small%20aged%20cheddar%20taste%20and%20save%20the%20creamery%20for%20future%20market%20orders.&cta=Claim%20cheddar%20bite&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-007&campaign=svfmpa-creekside-creamery&image=/ropebridge-core/assets/images/claim-image-arriving-soon.svg&image_alt=Creekside%20Creamery%20sample%20image%20placeholder"
+    destination: "/archetypes/claim/?vendor=Creekside%20Creamery&vendor_id=creekside-creamery&category=Cheese&title=Aged%20Cheddar%20Bite&description=Claim%20a%20small%20aged%20cheddar%20taste%20and%20save%20the%20creamery%20for%20future%20market%20orders.&cta=Claim%20cheddar%20bite&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-007&campaign=svfmpa-creekside-creamery&image=/assets/images/claim-image-arriving-soon.svg&image_alt=Creekside%20Creamery%20sample%20image%20placeholder"
   },
   "svfmpa-sp-008": {
     status: "live",
@@ -85,7 +85,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     vendorType: "pickles",
     vendorName: "Brine & Barrel",
     offerTitle: "Garlic Dill Pickle Chip",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=Brine%20%26%20Barrel&vendor_id=brine-and-barrel&category=Pickles&title=Garlic%20Dill%20Pickle%20Chip&description=Claim%20a%20crisp%20garlic%20dill%20pickle%20chip%20and%20save%20this%20small-batch%20brine%20maker%20for%20later.&cta=Claim%20pickle%20chip&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-008&campaign=svfmpa-brine-and-barrel&image=/ropebridge-core/assets/images/claim-image-arriving-soon.svg&image_alt=Brine%20and%20Barrel%20sample%20image%20placeholder"
+    destination: "/archetypes/claim/?vendor=Brine%20%26%20Barrel&vendor_id=brine-and-barrel&category=Pickles&title=Garlic%20Dill%20Pickle%20Chip&description=Claim%20a%20crisp%20garlic%20dill%20pickle%20chip%20and%20save%20this%20small-batch%20brine%20maker%20for%20later.&cta=Claim%20pickle%20chip&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-008&campaign=svfmpa-brine-and-barrel&image=/assets/images/claim-image-arriving-soon.svg&image_alt=Brine%20and%20Barrel%20sample%20image%20placeholder"
   },
   "svfmpa-sp-009": {
     status: "live",
@@ -94,7 +94,7 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     vendorType: "skincare",
     vendorName: "Willow Tallow & Soap",
     offerTitle: "Mini Balm Sample",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=Willow%20Tallow%20%26%20Soap&vendor_id=willow-tallow-and-soap&category=Skincare&title=Mini%20Balm%20Sample&description=Claim%20a%20mini%20balm%20sample%20or%20scent%20card%20and%20save%20this%20small-batch%20soap%20maker.&cta=Claim%20mini%20sample&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-009&campaign=svfmpa-willow-tallow-and-soap&image=/ropebridge-core/assets/images/claim-image-arriving-soon.svg&image_alt=Willow%20Tallow%20and%20Soap%20sample%20image%20placeholder"
+    destination: "/archetypes/claim/?vendor=Willow%20Tallow%20%26%20Soap&vendor_id=willow-tallow-and-soap&category=Skincare&title=Mini%20Balm%20Sample&description=Claim%20a%20mini%20balm%20sample%20or%20scent%20card%20and%20save%20this%20small-batch%20soap%20maker.&cta=Claim%20mini%20sample&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-009&campaign=svfmpa-willow-tallow-and-soap&image=/assets/images/claim-image-arriving-soon.svg&image_alt=Willow%20Tallow%20and%20Soap%20sample%20image%20placeholder"
   },
   "svfmpa-sp-010": {
     status: "live",
@@ -103,6 +103,6 @@ window.ROPEBRIDGE_GO_REGISTRY = {
     vendorType: "tea",
     vendorName: "Fieldnote Teas",
     offerTitle: "Iced Mint Tea Tasting",
-    destination: "/ropebridge-core/archetypes/claim/?vendor=Fieldnote%20Teas&vendor_id=fieldnote-teas&category=Tea&title=Iced%20Mint%20Tea%20Tasting&description=Claim%20a%20small%20iced%20mint%20tea%20taste%20and%20save%20this%20botanical%20tea%20vendor%20for%20later.&cta=Claim%20tea%20taste&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-010&campaign=svfmpa-fieldnote-teas&image=/ropebridge-core/assets/images/claim-image-arriving-soon.svg&image_alt=Fieldnote%20Teas%20sample%20image%20placeholder"
+    destination: "/archetypes/claim/?vendor=Fieldnote%20Teas&vendor_id=fieldnote-teas&category=Tea&title=Iced%20Mint%20Tea%20Tasting&description=Claim%20a%20small%20iced%20mint%20tea%20taste%20and%20save%20this%20botanical%20tea%20vendor%20for%20later.&cta=Claim%20tea%20taste&limit=One%20sample%20per%20person%2C%20per%20market%20day.&qr=svfmpa-sp-010&campaign=svfmpa-fieldnote-teas&image=/assets/images/claim-image-arriving-soon.svg&image_alt=Fieldnote%20Teas%20sample%20image%20placeholder"
   }
 };
